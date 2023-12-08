d2df4a89db9b6ad0e562036541818ba40dad467b
Saif Resun <resun.saif@gmail.com>
2023-12-08 23:42:00 +0600

    Update response.py

--- src/django_auth0_engine/response.py
+++ src/django_auth0_engine/response.py
@@ -1,14 +1,12 @@
 import pprint
 
 class AuthEngineResponse():
-	"""
-	AuthEngineResponse serves as a base class for various response objects
-	returned by methods within the AuthEngine and the ManagementEngine class.
-	It provides a standardized way to access response data and check for
-	successful operations.
-	
+	"""AuthEngineResponse serves as a base class for various response objects
+	returned by different methods of this module. It provides a standardized
+	way to access response data and check for successful operations.
+
 	Args:
-		**kwarg: keyword arguments of user's information
+		**kwarg: keyword argument containing information of the  response.
 	"""
 	def __init__(self, **kwarg) -> None:
 		self.access_token		:str | None
@@ -23,30 +21,38 @@ class AuthEngineResponse():
 		self.__dict__.update(**kwarg)
 
 	def __bool__(self) -> bool:
-		"""Returns the value of the private variable _bool. It's subclasses
-		defines the value of _bool depending on the result of an operation.
+		"""Returns the value of the private [1] variable _bool. Various subclasses
+		determine the value of _bool based on the outcome of an operation. Upon
+		successful operation, _bool must be set to True; otherwise, it remains
+		False. By default, _bool is initialized as False.
 		"""
 		return self._bool
 	
 	def __str__(self) -> str:
-		"""Returns a formatted string representing the response object. The string
-		is formatted using pprint.pformat().
+		"""Returns a formatted string containing all the public [1] properties of the
+		response object. The formatting utilizes the pprint.pformat() method.
 		"""
 		return pprint.pformat(dict(self))
 	
 	def __repr__(self) -> str:
-		"""Returns a formatted string representing all key-value pairs of the
-		response. The string is formatted using pprint.pformat().
+		"""Returns a formatted string containing all properties, including both
+		public [1] and private [1] ones, of the response. The formatting utilizes the
+		pprint.pformat() method.
 		"""
 		return pprint.pformat(self.__dict__)
 	
 	def __iter__(self):
-		"""Allows iterating over publicly accessible variables of the response
-		object. (Public variables:
-		https://docs.python.org/3/tutorial/classes.html#private-variables)
+		"""This method returns an iterator object, enabling iteration through the
+		public [1] variables of the response object.
 		"""
 		data = self.__dict__
 
 		for key in data:
 			if key[0] != '_':
 				yield (key, data[key])
+
+
+	"""
+	[1] Public and Private variables are defined here:
+	https://docs.python.org/3/tutorial/classes.html#private-variables
+	"""
