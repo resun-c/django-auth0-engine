9153b1e84da7cd6357c617650d6f2de01a7d7f69
Saif Resun <resun.saif@gmail.com>
2023-12-08 23:41:54 +0600

    Update middleware.py

--- src/django_auth0_engine/middleware.py
+++ src/django_auth0_engine/middleware.py
@@ -5,29 +5,35 @@ from django.utils.functional import SimpleLazyObject
 
 
 class SessionAuthMiddleware(MiddlewareMixin):
-    """This middleware authenticates users based on session cookies. When included
-    in the settings.MIDDLEWARE list, it automatically performs user
-    authentication for each request. Accessing request.user within your views
-    triggers the authentication process:
+    """This middleware authenticates the user by passing each request object to
+	the AuthEngine.authenticate() method. The authenticate() method retrieves
+	the ID token from the session data in the request and verifies it. Upon
+	validation, it returns either a User object or an AuthEngineError object.
+	This middleware assigns the returned object to the request.user attribute.
 
-    If a valid session cookie exists, a wrapped lambda function calls the
-    authenticate method from the AuthEngine, retrieving the user information.
+	The AuthEngine.authenticate() method is wrapped into a lambda function and
+	passed to SimpleLazyObject, and then the SimpleLazyObject is assigned to
+	the request.user attribute. This mechanism allows the authenticate() method
+	to only get invoked when the user property of the request is accessed. This
+	saves both resources and time.
 
-    If successful, the SimpleLazyObject returns a User instance, making it
-    readily available within your view logic.
-
-    If authentication fails or no session cookie is present, the
-    SimpleLazyObject returns an AuthEngineError.
+	If no session cookie is present, noting is done.
     """
     def process_request(self, request):
         if hasattr(request, "session") and cfg._SESSION_KEY in request.session:
             request.user = SimpleLazyObject(lambda: AuthEngine().authenticate(request))
 
 class HeaderAuthMiddleware(MiddlewareMixin):
-    """This middleware operates similarly to SessionAuthMiddleware, but it relies
-    on access tokens provided in the request header instead of session cookies.
-    The wrapped lambda function calls the authenticate_header method from the
-    AuthEngine, performing authentication based on the provided token.
+    """This middleware functions similarly to SessionAuthMiddleware, but it
+	employs the AuthEngine.authenticate_header() method. The
+	authenticate_header() method functions similarly to the
+	AuthEngine.authenticate() method, except that the ID token is parsed from
+	the Authorization header of the request object, rather than from the
+	session data.
+
+	Just like SessionAuthMiddleware it also uses the SimpleLazyObject as a
+	wrapper for the request.user attribute. This middleware is suitable for API
+	applications.
     """
     def process_request(self, request):
         if "Authorization" in request.headers:
